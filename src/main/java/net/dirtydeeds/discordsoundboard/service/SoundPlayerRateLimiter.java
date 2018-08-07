9841b30883a7a7d0b51e9f627786751da910b14e
Bradley Henry <brad.henry.49@gmail.com>
2018-08-07 14:12:18 -0400

    Add method for bypassing rate limit system for configured users

--- src/main/java/net/dirtydeeds/discordsoundboard/service/SoundPlayerRateLimiter.java
+++ src/main/java/net/dirtydeeds/discordsoundboard/service/SoundPlayerRateLimiter.java
@@ -7,16 +7,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class SoundPlayerRateLimiter {
 
     private static final Logger LOG = LoggerFactory.getLogger(SoundPlayerRateLimiter.class);
     private final int kSecondsToRestrict;
+    private final Set<String> kUnlimitedUsers;
     private final Cache<String, Instant> userAccessTimeCache;
 
     public SoundPlayerRateLimiter(DiscordSoundboardProperties properties) {
         kSecondsToRestrict = properties.getRateLimitRestrictDuration();
+        kUnlimitedUsers = properties.getUnlimitedUserIds();
         userAccessTimeCache = CacheBuilder.newBuilder()
                 .expireAfterWrite(kSecondsToRestrict, TimeUnit.SECONDS)
                 .removalListener(notification -> LOG.info("Removed from rate-limit list {}", notification.getKey()))
@@ -26,6 +29,9 @@ public class SoundPlayerRateLimiter {
     public boolean userIsRateLimited(String user) {
         final Instant instant = userAccessTimeCache.getIfPresent(user);
         if (instant == null) {
+            if (kUnlimitedUsers.contains(user)) {
+                return false;
+            }
             userAccessTimeCache.put(user, Instant.now());
             return false;
         } else {
