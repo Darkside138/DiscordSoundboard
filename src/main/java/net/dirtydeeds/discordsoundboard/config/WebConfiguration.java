0189f5dae0508d9fc29856e5450cbf1bcd29446f
Dave Furrer <davefurrer@gmail.com>
2025-12-09 18:32:29 -0600

    Trying to fix failure to load assets for new UI

--- src/main/java/net/dirtydeeds/discordsoundboard/config/WebConfiguration.java
+++ src/main/java/net/dirtydeeds/discordsoundboard/config/WebConfiguration.java
@@ -9,13 +9,13 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080",
-                        "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:8080",
+//                        "http://localhost:3000")
+//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true)
+//                .maxAge(3600);
     }
 
 }
